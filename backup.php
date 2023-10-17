fdfb52ac406450d9d630be1fc237511cb35730f8
Babak Ahari <babak.ahari@gmail.com>
2023-10-17 22:37:57 +0200

    Update backup.php
    remove space

--- backup.php
+++ backup.php
@@ -1,23 +1,19 @@
 <?php
 // Set timezone
 date_default_timezone_set('UTC');
-
 // Get current time to append to the backup file name
 $current_time = date("Y-m-d_H-i-s");
 $zipFileName = "Backup_" . $current_time . ".zip";
-
 // Create a new ZIP archive
 $zip = new ZipArchive();
 if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE) !== true) {
     exit("Could not open archive");
 }
-
 // Initialize an iterator to go through all files and folders in the current directory
 $files = new RecursiveIteratorIterator(
     new RecursiveDirectoryIterator('.'),
     RecursiveIteratorIterator::SELF_FIRST
 );
-
 // Loop through all the files and add them to the ZIP archive
 foreach ($files as $file) {
     $filename = $file->getFilename();
@@ -29,9 +25,7 @@ foreach ($files as $file) {
         }
     }
 }
-
 // Close the ZIP archive
 $zip->close();
-
 echo "ZIP archive created: " . $zipFileName;
 ?>
